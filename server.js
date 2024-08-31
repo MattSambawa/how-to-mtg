674181159589d27cac3e569b2dd0b68ad1d3558f
Matthew Sambawa <mattsambawa@Matthews-Laptop.local>
2024-08-30 18:38:19 -0700

    small fix to dir and single page

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve("frontend", cardGallery.html))
+    res.sendFile(path.resolve(__dirname, "frontend", cardGallery.html))
 });
 
 app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
