a347e2c1e00b0b485547a909d43f737d15298847
Matthew Sambawa <mattsambawa@Matthews-Laptop.local>
2024-08-31 14:21:27 -0700

    testing SPA server.js

--- frontend/static/js/cardGallery.js
+++ frontend/static/js/cardGallery.js
@@ -1,1 +1,23 @@
-console.log("JS is loaded!");
+const router = async() => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing Dashboard") },
+        { path: "/posts", view: () => console.log("Viewing Posts") },
+        { path: "/settings", view: () => console.log("Viewing Settings") },
+    ];
+
+    //Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    console.log(potentialMatches);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    router();
+});
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", cardGallery.html))
+    res.sendFile(path.resolve(__dirname, "frontend", "cardGallery.html"))
 });
 
-app.listen(process.env.PORT || 5060, () => console.log("Server running..."));
+app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
